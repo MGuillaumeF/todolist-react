46bb9bbc9796d709b949576c3c6146f92c8afc60
MGuillaumeF <michel.guillaumefoucaud@hotmail.fr>
2020-03-21 05:26:13 +0100

    Clean code

--- src/component/todo/Todo.tsx
+++ src/component/todo/Todo.tsx
@@ -3,10 +3,7 @@ import * as React from 'react';
 const EMPTY_STRING : string = '';
 const EMPTY_STRING_ARRAY : Array<string> = [];
 
-export interface ITodoProps {
-}
-
-export default function Todo (props: ITodoProps) {
+export default function Todo () {
     let [task, setTask] = React.useState(EMPTY_STRING);
     let [items, setItems] = React.useState(EMPTY_STRING_ARRAY);
 
@@ -18,7 +15,7 @@ export default function Todo (props: ITodoProps) {
     const onSubmit = (e : any) => {
         e.preventDefault();
         setItems([...items, task]);
-        setTask('');
+        setTask(EMPTY_STRING);
     };
     const onChange = (e : any) => {
         setTask(e.target.value);
@@ -29,9 +26,10 @@ export default function Todo (props: ITodoProps) {
                 <div className="card mb-3" key={index}>
                     <div className="card-body">
                         <h4>{item}
-                        <i className="fas fa-times"
-                        style={{cursor:'pointer', color : 'red', float : 'right'}}
-                        onClick={() => {onDelete(index)}}
+                        <i 
+                                className="fas fa-times"
+                            style={{cursor:'pointer', color : 'red', float : 'right'}}
+                            onClick={() => {onDelete(index)}}
                         ></i>
                         </h4>
                     </div>
@@ -40,28 +38,28 @@ export default function Todo (props: ITodoProps) {
         });
     };
   return (
-      <React.Fragment>
-    <div className='card my-3'>
-      <div className="card-header">
-          Todo List
-      </div>
-      <div className="card-body">
-          <form onSubmit={onSubmit}>
-            <div className="form-group">
-                <label htmlFor="element">Task</label>
-                <input 
-                    type="text"
-                    className='form-control form-control-lg' 
-                    name='element'
-                    onChange={onChange}
-                    value={task}
-                />
-            </div>
-            <button className="btn btn-primary btn-block">Add</button>
-          </form>
-      </div>
-    </div>
-    {renderTodo()}
+    <React.Fragment>
+        <div className='card my-3'>
+        <div className="card-header">
+            Todo List
+        </div>
+        <div className="card-body">
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="element">Task</label>
+                    <input 
+                        type="text"
+                        className='form-control form-control-lg' 
+                        name='element'
+                        onChange={onChange}
+                        value={task}
+                    />
+                </div>
+                <button className="btn btn-primary btn-block">Add</button>
+            </form>
+        </div>
+        </div>
+        {renderTodo()}
     </React.Fragment>
   );
 }
