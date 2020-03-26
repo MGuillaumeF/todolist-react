b4def37289c14590de712516869cbedc93d7773f
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-03-26 15:38:14 +0100

    Clean dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import Todo from './component/todo/Todo';
 
 function App() {
