717d9e565b51982972231a923c1c9d59d227164e
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-03-26 04:09:14 +0100

    Updates

---
+++ precache-manifest.3017acc6297755b5c64837dd7671cc9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d389ace76e9682391185bad6fff52a5",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "d17b2299354295a01dc2",
+    "url": "/todolist-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3a2dea2895ce50519694",
+    "url": "/todolist-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d17b2299354295a01dc2",
+    "url": "/todolist-react/static/js/2.881f2539.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react/static/js/2.881f2539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2dea2895ce50519694",
+    "url": "/todolist-react/static/js/main.f8ed2092.chunk.js"
+  },
+  {
+    "revision": "40448dada1caddeb56fb",
+    "url": "/todolist-react/static/js/runtime-main.c883ef73.js"
+  }
+]);
